85bb362a4ee0cf771743fef6885b23bcc26053b6
faizansk <faizansk814@gmail.com>
2023-05-20 18:35:18 +0530

    anonymys poll checking

--- routes/anonymys.routes.js
+++ routes/anonymys.routes.js
@@ -7,51 +7,52 @@ const { firebase } = require("../config/db");
 const fireDb = firebase.database();
 
 anonymysrouter.post("/vote", async (req, res) => {
-    const { pollId, selectedAnswers, pollData, pollName } = req.body;
-    const userId=ip.address()+pollId;
+  const { pollId, selectedAnswers } = req.body;
+  const userId=ip.address()+pollId;
     const pollRef = firebase.database().ref("polls/" + pollId);
-  
+
     pollRef.child("usersAttended").once("value", async function (snapshot) {
       const usersAttended = snapshot.val() || [];
-  
-      
-  
+
       if (Object.values(usersAttended).includes(userId.toString())) {
         res.status(208).send("User already voted for this poll");
       } else {
         pollRef.child("usersAttended").push(userId.toString());
-  
-        pollData.questions.forEach((question, questionId) => {
-          const options = question.options.filter(
-            (option, optionId) => selectedAnswers.find(a => a.questionId == questionId)?.optionsIds?.includes(optionId)
-          );
-  
-          options.forEach(option => {
-            option.votes++;
-            if (option.votedBy == null) {
-              option.votedBy = [];
-            }
-            option.votedBy.push({
-              email: "anonymous",
-              userId: userId,
-              fullName: "Anonymous",
+
+        pollRef.once("value", (snapshot) => {
+          const pollData = snapshot.val();
+
+          for (const selectedAnswer of selectedAnswers) {
+            const { questionId, optionsIds } = selectedAnswer;
+            const question = pollData.questions[questionId];
+            const options = optionsIds.map(
+              (optionId) => question.options[optionId]
+            );
+            options.map((option) => {
+              option.votes++;
+              if (option.votedBy == null) {
+                option.votedBy = [];
+              }
+              option.votedBy.push({
+                userId: userId,
+              });
             });
-          });
-  
-          question.totalVotes += options.length;
+            question.totalVotes++;
+          }
+
+          pollRef
+            .update(pollData)
+            .then(() => {
+              res.status(200).send("Vote recorded successfully");
+            })
+            .catch((error) => {
+              res.status(500).send(`Error recording vote: ${error}`);
+            });
         });
-  
-        pollRef
-          .update(pollData)
-          .then(() => {
-            res.status(200).send("Vote recorded successfully");
-          })
-          .catch((error) => {
-            res.status(500).send(`Error recording vote: ${error}`);
-          });
       }
     });
-  });
+  
+});
 
 
   module.exports=anonymysrouter
